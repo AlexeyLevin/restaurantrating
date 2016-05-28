879b3a6dd3b4bf2f286dd408dd75b6df9a98a9b6
AlexeyLevin <lay-way@ya.ru>
2016-05-28 18:27:16 +0300

    menu controller and test ready

--- src/test/java/com/alev/restaurantrating/web/menu/MenuRestControllerTest.java
+++ src/test/java/com/alev/restaurantrating/web/menu/MenuRestControllerTest.java
@@ -1,15 +1,21 @@
 package com.alev.restaurantrating.web.menu;
 
+import com.alev.restaurantrating.model.Menu;
 import com.alev.restaurantrating.service.MenuService;
+import com.alev.restaurantrating.util.json.JsonUtil;
 import com.alev.restaurantrating.web.AbstractControllerTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.alev.restaurantrating.ModelTestData.*;
 import static junit.framework.TestCase.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,95 +35,55 @@ public class MenuRestControllerTest extends AbstractControllerTest {
                 .andExpect(MENU_MATCHER.contentMatcher(RESTAURANT_1_MENU));
     }
 
-//    @Test
-//    public void testGetNotFound() throws Exception {
-//        mockMvc.perform(get(REST_URL + RESTAURANT_1_MENU_ID, RESTAURANT_2_ID))
-//                .andExpect(status().isNotFound());
-//    }
+    @Test
+    public void testGetNotFound() throws Exception {
+        mockMvc.perform(get(REST_URL + RESTAURANT_1_MENU_ID, RESTAURANT_2_ID))
+                .andExpect(status().isNotFound());
+    }
 
-//    @Test
-//    public void testDeleteNotFound() throws Exception {
-//        mockMvc.perform(delete(REST_URL + ADMIN_MEAL_ID)
-//                .with(userHttpBasic(USER)))
-//                .andDo(print())
-//                .andExpect(status().isNotFound());
-//    }
-//
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        mockMvc.perform(delete(REST_URL + RESTAURANT_3_MENU_ID, RESTAURANT_1_ID))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
     @Test
     public void testDelete() throws Exception {
-//        MENU_MATCHER.assertCollectionEquals(Collections.singletonList(RESTAURANT_1_MENU), menuService.getAll(RESTAURANT_1_ID));
         mockMvc.perform(delete(REST_URL + RESTAURANT_1_MENU_ID, RESTAURANT_1_ID))
                 .andExpect(status().isOk());
         assertTrue(menuService.getAll(RESTAURANT_1_ID).isEmpty());
     }
-//
-//    @Test
-//    public void testUpdate() throws Exception {
-//        UserMeal updated = getUpdated();
-//
-//        mockMvc.perform(put(REST_URL + MEAL1_ID).contentType(MediaType.APPLICATION_JSON)
-//                .content(JsonUtil.writeValue(updated))
-//                .with(userHttpBasic(USER)))
-//                .andExpect(status().isOk());
-//
-//        assertEquals(updated, service.get(MEAL1_ID, START_SEQ));
-//    }
-//
-//    @Test
-//    public void testCreate() throws Exception {
-//        UserMeal created = getCreated();
-//        ResultActions action = mockMvc.perform(post(REST_URL)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(JsonUtil.writeValue(created))
-//                .with(userHttpBasic(ADMIN)));
-//
-//        UserMeal returned = MENU_MATCHER.fromJsonAction(action);
-//        created.setId(returned.getId());
-//
-//        MENU_MATCHER.assertEquals(created, returned);
-//        MENU_MATCHER.assertCollectionEquals(Arrays.asList(ADMIN_MEAL2, created, ADMIN_MEAL), service.getAll(ADMIN_ID));
-//    }
-//
-//    @Test
-//    public void testGetAll() throws Exception {
-//        mockMvc.perform(get(REST_URL)
-//                .with(userHttpBasic(USER)))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MENU_MATCHER.contentListMatcher(UserMealsUtil.getWithExceeded(USER_MEALS, USER.getCaloriesPerDay())));
-//    }
-//
-//    @Test
-//    public void testGetBetween() throws Exception {
-//        mockMvc.perform(get(REST_URL + "between?startDateTime=2015-05-30T07:00&endDateTime=2015-05-31T11:00:00")
-//                .with(userHttpBasic(USER)))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(MENU_MATCHER.contentListMatcher(
-//                        UserMealsUtil.createWithExceed(MEAL4, true),
-//                        UserMealsUtil.createWithExceed(MEAL1, false)));
-//    }
-//
-//    @Test
-//    public void testFilter() throws Exception {
-//        mockMvc.perform(get(REST_URL + "filter?startDate=2015-05-30&startTime=07:00&endDate=2015-05-31&endTime=11:00")
-//                .with(userHttpBasic(USER)))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(MENU_MATCHER.contentListMatcher(
-//                        UserMealsUtil.createWithExceed(MEAL4, true),
-//                        UserMealsUtil.createWithExceed(MEAL1, false)));
-//    }
-//
-//    @Test
-//    public void testFilterAll() throws Exception {
-//        mockMvc.perform(get(REST_URL + "filter?startDate=&endTime=")
-//                .with(userHttpBasic(USER)))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(MENU_MATCHER.contentListMatcher(
-//                        UserMealsUtil.getWithExceeded(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), USER.getCaloriesPerDay())));
-//    }
-//
+
+    @Test
+    public void testUpdate() throws Exception {
+        Menu updated = new Menu(RESTAURANT_1_MENU_ID, RESTAURANT_1_NAME + STRING_MENU + " updated", VOTE_DAY);
+        mockMvc.perform(put(REST_URL + RESTAURANT_1_MENU_ID, RESTAURANT_1_ID).contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk());
+        assertEquals(updated, menuService.get(RESTAURANT_1_MENU_ID, RESTAURANT_1_ID));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Menu created = new Menu(RESTAURANT_1_NAME + STRING_MENU + " created", NEXT_VOTE_DAY);
+        ResultActions action = mockMvc.perform(post(REST_URL, RESTAURANT_1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(created)));
+
+        Menu returned = MENU_MATCHER.fromJsonAction(action);
+        created.setId(returned.getId());
+
+        MENU_MATCHER.assertEquals(created, returned);
+        MENU_MATCHER.assertCollectionEquals(Arrays.asList(created, RESTAURANT_1_MENU), menuService.getAll(RESTAURANT_1_ID));
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        mockMvc.perform(get(REST_URL, RESTAURANT_2_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_MATCHER.contentListMatcher(Collections.singletonList(RESTAURANT_2_MENU)));
+    }
 }
